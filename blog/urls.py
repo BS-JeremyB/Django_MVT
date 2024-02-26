784caa21b3dd8a5235b9a24a453fa9a6248ee23c
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-02-26 19:12:11 +0100

    url update

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view, name='home'),
     path('contact/', views.contact_view, name='contact'),
-    path('article/', include('article.urls'), name='article'),
+    path('article/', include('article.urls')),
 ]
