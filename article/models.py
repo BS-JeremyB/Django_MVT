88037565adfb454f03dcb749140a2a9e665b7572
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-02-27 12:03:38 +0100

    show title in admin

--- article/models.py
+++ article/models.py
@@ -6,3 +6,6 @@ class Article (models.Model):
     titre = models.CharField(max_length=150)
     contenu = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titre
