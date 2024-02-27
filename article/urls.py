35c2fbd145ce2dd7ebace671c07a59cf42aed0ec
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-02-27 16:07:29 +0100

    Static Files and POST

--- article/urls.py
+++ article/urls.py
@@ -6,5 +6,6 @@ app_name = "article"
 
 urlpatterns = [
     path('', views.list_view, name="articles"),
+    path('create/', views.create_view, name="create"),
     path('<int:id>/', views.detail_view, name="article")
 ]
--- article/views.py
+++ article/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
+
 from article.models import Article
+from .forms import ArticleForm
 
 
 def list_view(request):
@@ -11,3 +13,12 @@ def list_view(request):
 def detail_view(request, id):
     article = get_object_or_404(Article, id=id)
     return render(request, 'articles/detail.html', context={'article': article})
+
+def create_view(request):
+    form = ArticleForm()
+    if(request.method == "POST"):
+        form = ArticleForm(request.POST)
+        if(form.is_valid()):
+            form.save()
+            return redirect('article:articles')
+    return render(request, 'articles/create.html', context={'form': form})
